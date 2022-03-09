6bf72c50d71282bb589349c14bf0fec4eed8dfe4
ruby2800 <36958049+ruby2800@users.noreply.github.com>
2022-03-09 23:07:38 +0800

    complete hw3

--- credit_card.rb
+++ credit_card.rb
@@ -1,20 +1,30 @@
-require_relative './luhn_validator.rb'
-require 'json'
+# frozen_string_literal: true
 
+require_relative './luhn_validator'
+require 'json'
+# CreditCard
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
   # instance variables with automatic getter/setter methods
+  include LuhnValidator
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      'number' => @number,
+      'expiration_date' => @expiration_date,
+      'owner' => @owner,
+      'credit_network' => @credit_network
     }.to_json
   end
 
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# LuhnValidator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +8,18 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
+    # puts nums_a
     # TODO: use the integers in nums_a to validate its last check digit
+
+    all = 0
+    nums_a.reverse.each_slice(2) do |odd, even|
+      all += odd
+      if even.is_a? Integer
+        even *= 2
+        all += even / 10 >= 1 ? ((even % 10) + 1) : even
+      end
+    end
+    # puts all
+    (all % 10).zero? ? true : false
   end
 end
